374427c6a18eb30526757fa9a558c993e071fcd2
Stephan Burkhardt <stephan.burkhardt@googlemail.com>
2016-01-15 01:38:25 +0100

    Add some utility functions and their tests

--- src/helpers.py
+++ src/helpers.py
@@ -1,20 +1,26 @@
 import numpy as np
 
-def points_match(pA, pB, rad):
+
+def points_match(pA, pB, tol=1):
+    """Check whether two arrays of points contain (approximately) the same
+    points. Two points are approximately the same if their distance is
+    less than 'tol'. Behavior can get weird if multiple points are
+    close together.
+    """
     pA = np.array(pA)
     pB = np.array(pB)
 
     if len(pA) != len(pB):
         return False
-    
-    radSq = rad**2
+
+    tolSq = tol ** 2
 
     for val in pA:
-        dists = (pB - val)**2
-        euclid_dists = dists[:,0] + dists[:,1]
+        dists = (pB - val) ** 2
+        euclid_dists = dists[:, 0] + dists[:, 1]
         best_match = np.argmin(euclid_dists)
 
-        if euclid_dists[best_match] > radSq:
+        if euclid_dists[best_match] > tolSq:
             return False
         else:
             pB[best_match] = np.array([np.NaN, np.NaN])
@@ -22,6 +28,8 @@ def points_match(pA, pB, rad):
 
 
 def wu_sort_points(p1, p2, dim):
+    """This is a helper function for wu_sum in order to bring the points
+    into the right order."""
     switch_dim = abs(p1[0] - p2[0]) < abs(p1[1] - p2[1])
     if switch_dim:
         p1 = p1[::-1]
@@ -52,17 +60,18 @@ def wu_sum(array, p1, p2, count=False):
 
     if dir > 0:
         proper_y0 = int(np.floor(y0))
-        proper_y1 = int(np.ceil(y1))
+        proper_y1 = int(np.floor(y1))
     else:
         proper_y0 = int(np.ceil(y0))
-        proper_y1 = int(np.floor(y1))
+        proper_y1 = int(np.ceil(y1))
 
     if weight_first != 0.0:
         fst_offset = np.abs(proper_y0 - y0) - dy_by_dx * (1 - weight_first)
         fst_y0 = proper_y0
-        if fst_offset > 1:
-            fst_offset -= 1
-            fst_y0 += dir
+        assert(fst_offset < 1)
+        if fst_offset < 0:
+            fst_offset += 1
+            fst_y0 -= dir
         if switch_dim:
             s += (array[fst_y0, proper_x0 - 1] * (1 - fst_offset) +
                   array[fst_y0 + dir, proper_x0 - 1] * fst_offset) * weight_first
@@ -71,16 +80,17 @@ def wu_sum(array, p1, p2, count=False):
                   array[proper_x0 - 1, fst_y0 + dir] * fst_offset) * weight_first
 
     if weight_last != 0.0:
-        lst_offset = np.abs(proper_y1 - y1) + dy_by_dx * weight_last
+        lst_offset = np.abs(proper_y1 - y1) + dy_by_dx * (1 - weight_last)
+        assert(lst_offset > 0)
         if lst_offset > 1:
             lst_offset -= 1
             proper_y1 += dir
         if switch_dim:
-            s += (array[proper_y1 - dir, proper_x1 + 1] * (1 - lst_offset) +
-                  array[proper_y1, proper_x1 + 1] * lst_offset) * weight_last
+            s += (array[proper_y1, proper_x1 + 1] * (1 - lst_offset) +
+                  array[proper_y1 + dir, proper_x1 + 1] * lst_offset) * weight_last
         else:
-            s += (array[proper_x1 + 1, proper_y1 - dir] * (1 - lst_offset) +
-                  array[proper_x1 + 1, proper_y1] * lst_offset) * weight_last
+            s += (array[proper_x1 + 1, proper_y1] * (1 - lst_offset) +
+                  array[proper_x1 + 1, proper_y1 + dir] * lst_offset) * weight_last
 
     cur_offset = np.abs(proper_y0 - y0) + dy_by_dx * weight_first
     cur_y = proper_y0
@@ -89,22 +99,106 @@ def wu_sum(array, p1, p2, count=False):
 
     tmp_n = 0
     try:
-        for (ll, cur_x) in enumerate(range(x0, x1+1)):
+        for (ll, cur_x) in enumerate(range(x0, x1 + 1)):
             a = 0
             if cur_offset > 1:
                 cur_offset -= 1
                 cur_y += dir
             if switch_dim:
-                a = array[cur_y, cur_x] * (1 - cur_offset) + array[cur_y + dir, cur_x] * cur_offset
+                a = array[cur_y, cur_x] * (1 - cur_offset) + \
+                    array[cur_y + dir, cur_x] * cur_offset
             else:
-                a = array[cur_x, cur_y] * (1 - cur_offset) + array[cur_x, cur_y + dir] * cur_offset
+                a = array[cur_x, cur_y] * (1 - cur_offset) + \
+                    array[cur_x, cur_y + dir] * cur_offset
             if a != 0.0:
                 tmp_n += 1
             s += a
             cur_offset += dy_by_dx
 
-
     except IndexError:
         print("IndexError:", cur_x, cur_y, p1, p2, switch_dim, array.shape, s)
 
     return tmp_n if count else s
+
+
+def find_intersection(line1, line2):
+    """Find the intersection point between two lines. The lines are given
+    by a pair of points on them."""
+    x = line1[1] - line1[0]
+    y = line2[1] - line2[0]
+    c = line2[0] - line1[0]
+    if(min(abs(x[0]), abs(y[1])) < min(abs(x[1]), abs(y[0]))):
+        # To avoid divide by zero error, we switch the two lines
+        return find_intersection(line2, line1)
+    elif np.all([np.abs(a) < 1e-15 for a in [x[0], y[0]]]) or \
+         np.all([np.abs(a) < 1e-15 for a in [x[1], y[1]]]):
+        # This checks for parallel lines for which the following
+        # formulas would produce nasty warnings
+        return None
+    # Small derivation of the used formula:
+    # a * x + b * y = c
+    # a = (c[0] - b * y[0]) / x[0]
+    # b = (c[1] - a * x[1]) / y[1]
+    # b = (c[1] - (c[0] - b * y[0]) / x[0] * x[1]) / y[1]
+    # b * (1 - y[0] / x[0] * x[1] / y[1]) = c[1] / y[1] - c[0] * x[1] / x[0] / y[1]
+    # Silence 'division by zero' errors
+    old_err = np.geterr()['divide']
+    np.seterr(divide='ignore')
+    b = (c[1] / y[1] - c[0] * x[1] / x[0] / y[1]) / (1 - y[0] / x[0] * x[1] / y[1])
+    a = (c[0] - b * y[0]) / x[0]
+    np.seterr(divide=old_err)
+    return np.array(line1[0] + a * (line1[1] - line1[0])) \
+        if not np.any([np.isinf(coeff) or np.isnan(coeff) for coeff in [a, b]]) else None
+
+
+def find_intersections(line1, otherlines):
+    """Find intersections between a line and a set of other lines."""
+    return [find_intersection(line1, ol) for ol in otherlines]
+
+
+def order_points(points):
+    """Orders points (given as list of coordinate tuples) that lie on a line.
+    Tries to preserve order. Gives reordering as tuple of indices."""
+    line_vec = points[1] - points[0]
+    origin = points[0]
+    distances = [np.dot(p - origin, line_vec) for p in points]
+    return np.argsort(distances)
+
+
+def get_corners(lines):
+    """Find the corners of a rectangle given by four lines. Probably
+    crashes if two lines are exactly parallel."""
+    # Look for parallel lines first
+    lines = [np.array(l) for l in lines]
+    parallels = [np.argwhere([find_intersection(lines[jj], lines[ii]) is None
+                             for jj in range(ii + 1, len(lines))])
+                 for ii in range(len(lines))]
+    if np.any([len(p) > 1 for p in parallels]):
+        return None
+    par_lines = np.argwhere([len(p) > 0 for p in parallels])
+    if len(par_lines) > 0:
+        l1 = par_lines[0, 0]
+        l2 = parallels[l1][0, 0] + l1 + 1
+        line1 = lines[l1]
+        line2 = lines[l2]
+        lines.pop(max(l1, l2))
+        lines.pop(min(l1, l2))
+        lines = [line1, line2] + lines
+
+    # If we had any parallel lines, they are now in position 0 and 1
+    l = lines[0]
+    otherlines = lines[1:]
+    for (ii, ol) in enumerate(otherlines):
+        inter = find_intersection(l, ol)
+        rest = otherlines.copy()
+        del rest[ii]
+        if inter is None:
+            break
+        inters1 = [inter] + find_intersections(l, rest)
+        inters2 = [inter] + find_intersections(ol, rest)
+        if np.all(order_points(inters1) == order_points(inters2)):
+            # The two lines are opposite in the rectangle
+            break
+    lines = [l, rest[0], ol, rest[1]]
+    corners = [find_intersection(lines[ii], lines[(ii + 1) % 4]) for ii in range(4)]
+    return (corners, lines)
--- src/test.py
+++ src/test.py
@@ -3,19 +3,21 @@ import rectangle_finder
 import unittest
 import numpy as np
 
+
 class test_helpers(unittest.TestCase):
-    def test_pointers_match(self):
-        '''Test whether the match function does indeed return true if the points are matched within the tolerance'''
-        values = [ ([[1.2, 1.4]], [[1.3, 1.4]], 0.11, True),
-                   ([[1.2, 1.4], [0.1, 2.9]], [[0.0, 3.0], [1.2, 1.6]], 0.25, True),
-                   ([[1.2, 1.4]], [[0.0, 3.0], [1.2, 1.6]], 0.25, False),
-                   ([[1.2, 1.4], [0.1, 2.9]], [[0.0, 3.0], [1.3, 1.45]], 0.1, False)]
+    def test_points_match(self):
+        """Test whether the match function does indeed return true if the
+        points are matched within the tolerance"""
+        values = [([[1.2, 1.4]], [[1.3, 1.4]], 0.11, True),
+                  ([[1.2, 1.4], [0.1, 2.9]], [[0.0, 3.0], [1.2, 1.6]], 0.25, True),
+                  ([[1.2, 1.4]], [[0.0, 3.0], [1.2, 1.6]], 0.25, False),
+                  ([[1.2, 1.4], [0.1, 2.9]], [[0.0, 3.0], [1.3, 1.45]], 0.1, False)]
         for pA, pB, rad, res_val in values:
             result = helpers.points_match(pA, pB, rad)
             self.assertEqual(res_val, result)
 
     def test_wu_sum(self):
-        '''Test whether the wu-sum does what it is supposed to do.'''
+        """Test whether the wu-sum does what it is supposed to do."""
         tol = 1e-13
         test_array = np.random.random(size=(200, 200))
 
@@ -32,18 +34,6 @@ class test_helpers(unittest.TestCase):
         val2c = np.sum(test_array[10:111, 5]) + np.sum(test_array[10:111, 6])
         self.assertAlmostEqual(val2, val2c, delta=tol)
 
-        # Test whether everything still works if we have a non-integer start x
-        val3 = helpers.wu_sum(test_array, (9.5, 5), (110, 6)) + \
-               helpers.wu_sum(test_array, (9.5, 6), (110, 5))
-        val3c = val2c + np.sum(test_array[9, 5:7]) * 0.5
-        self.assertAlmostEqual(val3, val3c, delta=tol)
-
-        # Test a non-integer stop x
-        val4 = helpers.wu_sum(test_array, (10, 5), (110.5, 6)) + \
-               helpers.wu_sum(test_array, (10, 6), (110.5, 5))
-        val4c = val2c + np.sum(test_array[111, 5:7]) * 0.5
-        self.assertAlmostEqual(val4, val4c, delta=tol)
-
         # Test non-integer start and stop y
         val5 = helpers.wu_sum(test_array, (10, 5.1), (110, 5.9)) + \
                helpers.wu_sum(test_array, (10, 5.9), (110, 5.1))
@@ -53,13 +43,13 @@ class test_helpers(unittest.TestCase):
         # Test non-integer start and stop y as well as non-integer start x
         val6 = helpers.wu_sum(test_array, (9.5, 5.1), (110, 5.9)) + \
                helpers.wu_sum(test_array, (9.5, 5.9), (110, 5.1))
-        val6c = val3c
+        val6c = val2c + np.sum(test_array[9, 5:7]) * 0.5
         self.assertAlmostEqual(val6, val6c, delta=tol)
 
         # Test non-integer stop x with non-integer start and stop y
         val7 = helpers.wu_sum(test_array, (10, 5.1), (110.5, 5.9)) + \
                helpers.wu_sum(test_array, (10, 5.9), (110.5, 5.1))
-        val7c = val4c
+        val7c = val2c + np.sum(test_array[111, 5:7]) * 0.5
         self.assertAlmostEqual(val7, val7c, delta=tol)
 
         # Test whether summing either row-wise or column-wise makes a
@@ -72,6 +62,51 @@ class test_helpers(unittest.TestCase):
         val2_y_l = helpers.wu_sum(test_array, (20.3, 10.3), (120.1, 110.1 - tol))
         self.assertAlmostEqual(val2_x_l, val2_y_l, delta=10 * tol)
 
+    def test_find_intersection(self):
+        val_1_f = helpers.find_intersection(np.array([[1, 0], [0, 1]]),
+                                            np.array([[0, 0], [2, 0]]))
+        val_1_c = np.array([1, 0])
+        self.assertTrue(np.sum(np.abs((val_1_f - val_1_c))) < 1e-13)
+
+        val_2_f = helpers.find_intersection(np.array([[1, 0], [0, 1]]),
+                                            np.array([[-1, 0], [0, -1]]))
+        self.assertIsNone(val_2_f)
+
+        val_3_f = helpers.find_intersection(np.array([[0, 1], [0, 2]]),
+                                            np.array([[1, 0], [2, 0]]))
+        val_3_c = np.array([0, 0])
+        self.assertTrue(np.sum(np.abs((val_3_f - val_3_c))) < 1e-13)
+
+        line_1 = np.random.rand(2, 2)
+        line_2 = np.random.rand(2, 2)
+        val_4_1 = helpers.find_intersection(line_1, line_2)
+        val_4_2 = helpers.find_intersection(line_2, line_1)
+        self.assertTrue(np.sum(np.abs((val_4_1 - val_4_2))) < 1e-13)
+
+    def test_get_corners(self):
+        [val_1, l_s] = helpers.get_corners([np.array(a) for a in
+                                     [[[0, 0], [0, 1.1]],
+                                      [[0, 1.1], [1, 1]],
+                                      [[1, 1], [1.1, 0]],
+                                      [[1.1, 0], [0, 0]]]])
+        corners_c_1 = [[0, 0], [1.1, 0], [1, 1], [0, 1.1]]
+        self.assertTrue(helpers.points_match(val_1, corners_c_1, tol=1e-13))
+
+        [val_2, l_s] = helpers.get_corners([np.array(a) for a in
+                                     [[[0, 0], [0, 1]],
+                                      [[0, 1], [1, 1]],
+                                      [[1, 1], [1, 0]],
+                                      [[1, 0], [0, 0]]]])
+        corners_c_2 = [[0, 0], [1, 0], [1, 1], [0, 1]]
+        self.assertTrue(helpers.points_match(val_2, corners_c_2, tol=1e-13))
+
+        
+# class test_helpers(unittest.TestCase):
+#     def test_refine_line(self):
+#         pass
+
+#     def test_overlap_sum(self):
+#         pass
         
 if __name__ == "__main__":
     unittest.main()
