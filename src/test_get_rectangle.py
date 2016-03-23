08487be3b3f207173cc3ea35c2700886e829a40d
Stephan Burkhardt <stephan.burkhardt@googlemail.com>
2016-03-23 10:46:52 +0100

    Add some debugging to test_get_rectangle

--- src/test_get_rectangle.py
+++ src/test_get_rectangle.py
@@ -12,7 +12,7 @@ import argparse as ap
 
 def main_fnct(filen):
     if filen == "":
-        filen='../test_whiteboards/cellphone_samples/big_blackboard_blank.jpg'
+        filen = '../test_whiteboards/cellphone_samples/big_blackboard_blank.jpg'
     sample_image = rf.loadimage(filen)
     # cannied_sample = rf.auto_canny(rf.desaturate(sample_image), sigma=0.1)
     # plt.subplot(221)
@@ -27,11 +27,14 @@ def main_fnct(filen):
     # plt.imshow(contour_image, cmap=plt.cm.Greys_r)
     # plt.show()
     t = time.time()
-    test_rect = rf.get_rectangle_from_image(sample_image)
+    debug = False
+    test_rect = rf.get_rectangle_from_image(sample_image, debug=debug)
+    if debug:
+        plt.show()
     print(time.time() - t)
     print(test_rect)
     x = rf.estimate_aspect_ratio_fit(test_rect, sample_image.shape[0:2])
-    print(x[0])
+    print('Estimated aspect:', x[0], 1.0 / x[0])
     fit_rect = x[1]
     print(fit_rect)
     plt.imshow(sample_image)
