a1ac91de742a654dc2ac3fd08a2f5845d3397a0d
Stephan Burkhardt <stephan.burkhardt@googlemail.com>
2016-09-11 17:55:48 +0200

    Nicer output of test_get_rectangle

--- src/test_get_rectangle.py
+++ src/test_get_rectangle.py
@@ -31,12 +31,12 @@ def main_fnct(filen):
     test_rect = rf.get_rectangle_from_image(sample_image, debug=debug)
     if debug:
         plt.show()
-    print(time.time() - t)
-    print(test_rect)
+    print("Time taken for rectangle detection:", time.time() - t)
+    #print(test_rect)
     x = rf.estimate_aspect_ratio_fit(test_rect, sample_image.shape[0:2])
-    print('Estimated aspect:', x[0], 1.0 / x[0])
+    print('Estimated aspect ratio:', x[0], 1.0 / x[0])
     fit_rect = x[1]
-    print(fit_rect)
+    #print(fit_rect)
     plt.imshow(sample_image)
     plt.scatter(test_rect[:, 1], test_rect[:, 0], c=['r', 'g', 'b', 'y'], s=80)
     plt.scatter(fit_rect[:, 1], fit_rect[:, 0], c=['r', 'g', 'b', 'y'], s=40)
